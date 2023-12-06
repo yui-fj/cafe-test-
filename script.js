d3b982d326eeb073b8be4b756eade39fb0296f03
yui-fj <145320025+yui-fj@users.noreply.github.com>
2023-12-06 16:39:05 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// //一度表示したら動作を止める
+// const show = (entries, obs) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting){
+//         const keyframes = {
+//           opacity: [0, 1],
+//           translate: ['0 200px',0]
+//         }
+//         entry.target.animate(keyframes,600);
+  
+//         //一度表示されたら止める
+//         obs.unobserve(entry.target);
+//       }
+//     });
+//   }
+  
+//   const observer = new IntersectionObserver(show);
+  
+//   //監視対象
+//   const images = document.querySelectorAll('.img');
+//   images.forEach(img =>{
+//     observer.observe(img);
+//   });
+
+
+  
